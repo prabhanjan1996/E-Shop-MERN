384b7c54ee15f6f6b16a3c1b5b705add38b80d64
Prabhanjan Desai <desaiprabhanjan05@gmail.com>
2021-02-26 10:43:50 +0530

    Order API creation

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -12,4 +12,27 @@ router.get(`/`, async  (req, res) =>{
     res.send(orderList);
 })
 
+// API for Insert orders values 
+router.post('/', async (req,res)=>{
+    
+
+    let order = new Order({
+        orderItems: req.body.orderItems,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user
+    })
+    order = await order.save();
+
+    if(!order)
+    return res.status(404).send('the oder cannot be created!')
+    res.send(oder);
+})
+
 module.exports = router;
